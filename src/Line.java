5d6446073d39e9008d214403c417322cff2d3458
kkm0510 <kartikeya.maan1997@yahoo.com>
2023-03-01 18:29:25 +0530

    REFACTORED : reduced one class

--- src/Line.java
+++ src/Line.java
@@ -9,4 +9,28 @@ public class Line {
         point2 = new Point();
         length = Math.sqrt(Math.pow(point2.x - point1.x, 2.0) + (Math.pow((point2.y - point1.y), 2.0)));
     }
+
+    public boolean equals(Line line) {
+        return this.length == line.length;
+    }
+
+    public int compareTo(Line line) {
+        return (int) (this.length - line.length);
+    }
+
+    public static void main(String[] args) {
+        Line line1 = new Line();
+        Line line2 = new Line();
+        if (line1.equals(line2))
+            System.out.println("lines are equal");
+        else
+            System.out.println("lines not equal");
+
+        if (line1.compareTo(line2) == 0)
+            System.out.println("lines are equal");
+        else if (line1.compareTo(line2) < 0)
+            System.out.println("line1 is less than line2");
+        else
+            System.out.println("line1 is greater than line2");
+    }
 }
